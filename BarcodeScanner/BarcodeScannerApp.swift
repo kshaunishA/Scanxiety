b1a0309a53ebac30a0946ebd13e97417b9b64288
Kshaunish Addala <kaddala@uwaterloo.ca>
2025-07-29 19:45:21 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Kshaunish Addala on 2025-07-29.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BarcodeScannerTests/BarcodeScannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerTests.swift
+//  BarcodeScannerTests
+//
+//  Created by Kshaunish Addala on 2025-07-29.
+//
+
+import Testing
+@testable import BarcodeScanner
+
+struct BarcodeScannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
